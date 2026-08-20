10f78242ff14d151d15aaec500b260536869ace7
scincedata12-hash <scincedata12@gmail.com>
2026-08-21 03:13:00 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="credit-card-assesment.c5qagcekwsqw.eu-north-1.rds.amazonaws.com",
+        host="credit-card-assesment-db.c5qagcekwsqw.eu-north-1.rds.amazonaws.com",
         port=5432,
         database="customer",
         user="postgres",
